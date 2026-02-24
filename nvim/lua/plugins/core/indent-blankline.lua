468e1161128b23dcc2a15acd91495225eba17ad2
helvnor <henrik@elvinnorrman.com>
2026-02-24 13:33:37 +0100

    feat(nvim): add indent blankline plugin

---
+++ nvim/lua/plugins/core/indent-blankline.lua
@@ -0,0 +1,13 @@
+return {
+	"lukas-reineke/indent-blankline.nvim",
+	enabled = true,
+	event = { "BufReadPost", "BufNewFile" },
+	main = "ibl",
+	---@module "ibl"
+	---@type ibl.config
+	opts = {
+		indent = {
+			char = "│",
+		},
+	},
+}
