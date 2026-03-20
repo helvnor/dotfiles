12ef33088953cb29001f3939b947dd3afba029ab
helvnor <henrik@elvinnorrman.com>
2026-03-20 16:35:02 +0100

    feat(fish): split functions to separate files

--- fish/functions/pack.fish
+++ fish/functions/pack.fish
@@ -22,33 +22,3 @@ function pack
     return 1
   end
 end
-
-function unpack
-  if not set -q argv[1]
-    echo "Usage: unpack <filename>"
-    return 1
-  end
-
-  set filename $argv[1]
-  set encrypted_file "$filename.tar.gz.age"
-
-  if not test -f $encrypted_file
-    echo "Error: Encrypted file '$encrypted_file' does not exist."
-    return 1
-  end
-
-  mkdir -p $filename
-  pushd $filename
-  age -d -o - ../$encrypted_file | tar -xzvf -
-  set status_code $status
-  popd
-
-  if test $status_code -eq 0
-    echo "Successfully decrypted and extracted archive: $encrypted_file"
-  else
-    echo "Error: Failed to decrypt and extract archive."
-    rm -rf $filename
-    return 1
-  end
-end
-
