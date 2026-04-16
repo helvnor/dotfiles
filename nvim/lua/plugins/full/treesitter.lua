1655f901e3531a4e6c9e1022b24c67befac575f1
helvnor <henrik@elvinnorrman.com>
2026-04-16 12:24:10 +0200

    fix(nvim): fix treesitter

--- nvim/lua/plugins/full/treesitter.lua
+++ nvim/lua/plugins/full/treesitter.lua
@@ -5,14 +5,9 @@ return {
 	build = ":TSUpdate",
 	event = { "BufReadPost", "BufNewFile" },
 	config = function()
-		local configs = require("nvim-treesitter.configs")
 		local servers = require("config.servers")
-		configs.setup({
+		require("nvim-treesitter").setup({
 			ensure_installed = servers.parsers,
-			sync_install = false,
-			highlight = { enable = true },
-			indent = { enable = true },
-			fold = { enable = true },
 		})
 	end,
 }
