5e8038ed57f9bffcaae4c38d55957e6a7d9066bd
helvnor <henrik@elvinnorrman.com>
2026-04-16 12:35:38 +0200

    fix(nvim): fix telescope

--- nvim/lua/plugins/core/telescope.lua
+++ nvim/lua/plugins/core/telescope.lua
@@ -2,11 +2,17 @@ return {
 	"nvim-telescope/telescope.nvim",
 	enabled = true,
 	cmd = "Telescope",
-	tag = "0.1.8",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-telescope/telescope-ui-select.nvim",
 	},
+	init = function()
+		if not vim.treesitter.ft_to_lang then
+			vim.treesitter.ft_to_lang = function(ft)
+				return vim.treesitter.language.get_lang(ft) or ft
+			end
+		end
+	end,
 	opts = {
 		extensions = {
 			fzf = {
